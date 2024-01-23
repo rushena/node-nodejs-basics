383b7c6b0e5c3fa5458ac38b15a016857cd3f656
Rushen Adjigeldieva <rushena2706@gmail.com>
2024-01-23 22:56:15 +0500

    feat: added fs/create task solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  const folderPath = path.dirname(process.argv[1]);
+  const filePath = path.join(folderPath, 'files', 'fresh.txt');
+
+  const fileStream = createWriteStream(filePath, {
+    flags: 'ax'
+  });
+
+  fileStream.on('error', () => {
+    throw Error('FS operation failed');
+  })
+
+  fileStream.write('I am fresh and young');
+
+  fileStream.end();
 };
 
 await create();
