0a67ac7087e9eeb21da08edf29e7e14b249e03b4
Rushen Adjigeldieva <rushena2706@gmail.com>
2024-01-24 09:52:26 +0500

    feat: added zip/compress task solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import {createGzip} from 'zlib';
+import {createWriteStream, createReadStream} from 'fs';
+import path from 'path';
+
 const compress = async () => {
-    // Write your code here 
+  const folderPath = path.dirname(process.argv[1]);
+  const filePath = path.join(folderPath, 'files', 'fileToCompress.txt');
+  const zipFile = path.join(folderPath, 'files', 'archive.gz')
+
+  const fileRead = createReadStream(filePath);
+  const writeZip = createWriteStream(zipFile);
+  const gzip = createGzip();
+
+  fileRead.pipe(gzip).pipe(writeZip);
 };
 
 await compress();
