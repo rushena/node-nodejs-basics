f7e1a920faa02ec5374fdc77cb4eee8ce4be8a01
Rushen Adjigeldieva <rushena2706@gmail.com>
2024-01-24 10:35:41 +0500

    feat: added streams/write task solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import {createWriteStream} from 'fs';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const folderPath = path.dirname(process.argv[1]);
+  const filePath = path.join(folderPath, 'files', 'fileToWrite.txt');
+  const writeStream = createWriteStream(filePath);
+  process.stdin.pipe(writeStream);
 };
 
 await write();
