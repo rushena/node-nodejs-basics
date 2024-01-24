266f14e1fa0df83c17139b68a4e368c57681cdc2
Rushen Adjigeldieva <rushena2706@gmail.com>
2024-01-24 11:07:37 +0500

    feat: added hash/calcHash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import {createReadStream} from 'fs';
+import stream from 'stream';
+import {createHash} from 'crypto';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const folderPath = path.dirname(process.argv[1]);
+  const filePath = path.join(folderPath, 'files', 'fileToCalculateHashFor.txt');
+
+  const readFile = createReadStream(filePath);
+  const transformFile = new stream.Transform({
+    transform(chunk, encoding, callback) {
+      const res = createHash('sha256').update(chunk.toString()).digest('hex');
+      callback(null, res);
+    }
+  });
+
+  readFile.pipe(transformFile).pipe(process.stdout);
+  readFile.on('end', () => {process.stdout.write('\n')});
 };
 
 await calculateHash();
